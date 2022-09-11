16423731b51e0d5f0575c2fb925243d76d1bdd75
famima65536 <famima65536@gmail.com>
2022-09-11 01:22:59 +0000

    componentsに相当する部分をまとめるクラスを作る

---
+++ src/Root.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace oirancage\HolidayCreatorItemStructure;
+
+use oirancage\HolidayCreatorItemStructure\component\ItemProperties;
+use oirancage\HolidayCreatorItemStructure\component\RenderOffsets;
+use oirancage\HolidayCreatorItemStructure\utils\Encodable;
+use pocketmine\nbt\tag\CompoundTag;
+
+class Root implements Encodable{
+
+    private ?RenderOffsets $renderOffsets = null;
+
+    public function __construct(
+        private ItemProperties $itemProperties
+    ){
+    }
+
+    /**
+     * @return $this
+     */
+    public function setRenderOffsets(RenderOffsets $renderOffsets) : self{
+        $this->renderOffsets = $renderOffsets;
+        return $this;
+    }
+
+    public function encode() : CompoundTag{
+        $components = CompoundTag::create();
+
+        $components->setTag($this->itemProperties->getName(), $this->itemProperties->encode());
+
+        if($this->renderOffsets !== null){
+            $components->setTag($this->renderOffsets->getName(), $this->renderOffsets->encode());
+        }
+
+        return $components;
+    }
+}
