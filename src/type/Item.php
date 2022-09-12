14ea407cec9539787ec5e10629aef03664770a95
famima65536 <famima65536@gmail.com>
2022-09-12 04:01:12 +0000

    Item: create method

--- src/type/Item.php
+++ src/type/Item.php
@@ -8,6 +8,12 @@ class Item implements IType{
 
     public string $name;
 
+    public static function create(string $name) : self{
+        $result = new self;
+        $result->name = $name;
+        return $result;
+    }
+
     private const NAME = "name";
 
     public function encode(): CompoundTag{
