784175e1cb3c88f61c89bff35166b8a5fb9ae154
famima65536 <famima65536@gmail.com>
2022-09-21 23:33:23 +0000

    all some more components

--- src/Root.php
+++ src/Root.php
@@ -2,16 +2,22 @@
 
 namespace oirancage\HolidayCreatorItemStructure;
 
+use oirancage\HolidayCreatorItemStructure\component\Cooldown;
+use oirancage\HolidayCreatorItemStructure\component\Fuel;
 use oirancage\HolidayCreatorItemStructure\component\ItemProperties;
 use oirancage\HolidayCreatorItemStructure\component\RenderOffsets;
 use oirancage\HolidayCreatorItemStructure\component\Shooter;
+use oirancage\HolidayCreatorItemStructure\component\Wearable;
 use oirancage\HolidayCreatorItemStructure\utils\Encodable;
 use pocketmine\nbt\tag\CompoundTag;
 
 class Root implements Encodable{
 
+    private ?Cooldown $cooldown = null;
+    private ?Fuel $fuel = null;
     private ?RenderOffsets $renderOffsets = null;
     private ?Shooter $shooter = null;
+    private ?Wearable $wearable = null;
 
     public function __construct(
         private ItemProperties $itemProperties
@@ -38,22 +44,15 @@ class Root implements Encodable{
         return $this;
     }
 
-    private const ITEM_PROPERTIES = "item_properties";
-    private const MINECRAFT_RENDER_OFFSETS = "minecraft:render_offsets";
-    private const MINECRAFT_SHOOTER = "minecraft:shooter";
-
     public function encode() : CompoundTag{
         $components = CompoundTag::create();
 
-        $components->setTag(self::ITEM_PROPERTIES, $this->itemProperties->encode());
-
-        if($this->renderOffsets !== null){
-            $components->setTag(self::MINECRAFT_RENDER_OFFSETS, $this->renderOffsets->encode());
-        }
-        
-        if($this->shooter !== null){
-            $components->setTag(self::MINECRAFT_SHOOTER, $this->shooter->encode());
-        }
+        $this->itemProperties->write($components);
+        $this->cooldown?->write($components);
+        $this->fuel?->write($components);
+        $this->renderOffsets?->write($components);
+        $this->shooter?->write($components);
+        $this->wearable?->write($components);
 
         return $components;
     }
